4d54f2754bb174ef6c8c434736e0141d8e3bebfa
Nemo Feng <nemofq@gmail.com>
2025-04-02 20:07:13 -0700

    Added 10-second cache revalidation interval to avoid render limitation of Vercel's free plan

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import { list } from '@vercel/blob';
 import ClientHome from './client';
-import { unstable_noStore } from 'next/cache';
+
+export const revalidate = 10;
 
 export default async function Home() {
-  unstable_noStore();
-  
   const { blobs } = await list({ prefix: process.env.BLOB_PATH });
   const timestamp = blobs.length > 0 ? (blobs[0]?.updatedAt || blobs[0]?.uploadedAt) : null;
 
